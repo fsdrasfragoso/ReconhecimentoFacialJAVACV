8ed894f488635dbce806bd02c26e4604addd579d
fsdrasfragoso <esdrasfragoso@gmail.com>
2018-03-03 17:03:07 -0300

    Detecção de Faces executada com sucesso!

--- ReconhecimentoFacialJAVACV/src/reconhecimento/Captura.java
+++ ReconhecimentoFacialJAVACV/src/reconhecimento/Captura.java
@@ -6,8 +6,17 @@
 package reconhecimento;
 
 import java.awt.event.KeyEvent;
+import org.bytedeco.javacpp.opencv_core.Mat;
+import org.bytedeco.javacpp.opencv_core.Rect;
+import org.bytedeco.javacpp.opencv_core.RectVector;
+import org.bytedeco.javacpp.opencv_core.Scalar;
+import org.bytedeco.javacpp.opencv_core.Size;
 import org.bytedeco.javacpp.opencv_face;
 import static org.bytedeco.javacpp.opencv_face.createEigenFaceRecognizer;
+import static org.bytedeco.javacpp.opencv_imgproc.COLOR_BGRA2GRAY;
+import static org.bytedeco.javacpp.opencv_imgproc.cvtColor;
+import static org.bytedeco.javacpp.opencv_imgproc.rectangle;
+import org.bytedeco.javacpp.opencv_objdetect.CascadeClassifier;
 import org.bytedeco.javacv.CanvasFrame;
 import org.bytedeco.javacv.Frame;
 import org.bytedeco.javacv.FrameGrabber;
@@ -26,11 +35,24 @@ public class Captura {
         OpenCVFrameConverter.ToMat  converteMat = new OpenCVFrameConverter.ToMat();
         OpenCVFrameGrabber camera = new OpenCVFrameGrabber(0); 
         camera.start();
+        CascadeClassifier detectorFace= new CascadeClassifier("src\\recursos\\haarcascade-frontalface-alt.xml");
         
-        CanvasFrame cFrame = new CanvasFrame("Previw",CanvasFrame.getDefaultGamma() / camera.getGamma()); 
+        CanvasFrame cFrame = new CanvasFrame("Preview",CanvasFrame.getDefaultGamma() / camera.getGamma()); 
         
         Frame  frameCapturado = null; 
+        
+        Mat imagemColorida = new Mat(); 
+        
         while((frameCapturado = camera.grab()) != null){
+            imagemColorida = converteMat.convert(frameCapturado);
+            Mat imagemCinza = new Mat();
+            cvtColor(imagemColorida, imagemCinza, COLOR_BGRA2GRAY);
+            RectVector facesDetectadas = new RectVector(); 
+            detectorFace.detectMultiScale(imagemCinza, facesDetectadas, 1.1, 1, 0, new Size(150,150), new Size(500,500));
+            for (int i=0; i < facesDetectadas.size(); i++){
+                Rect dadosFace = facesDetectadas.get(0);
+                rectangle(imagemColorida, dadosFace, new Scalar(0,0,255,0));
+            }
             if(cFrame.isVisible()){
                 cFrame.showImage(frameCapturado);
             }
